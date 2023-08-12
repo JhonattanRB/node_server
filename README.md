db153292af6715b4df39350a2994521c54d80f71
Jhonattan Rodriguez <jhonattan.jrodriguezb@gmail.com>
2023-08-12 14:11:59 -0500

    lab promises

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 ## Taks List in JS
 
 You can add task, remove task and complete task.
+
+#### Part 2. Lista de tareas asíncrona en node
+
+1. ¿Qué sucedio al usar async y await?
+
+En este caso, al usar await en funciones como addTask, removeTask, y completeTask, el programa esperó a que el usuario completara la acción antes de continuar con el siguiente paso.
+
+2. ¿Qué sucedio al usar el método then()?
+
+Al usar el método then(), estamos agregando un callback que se ejecutará una vez que la promesa se haya resuelto...después de llamar a addTask, removeTask, o completeTask, el programa esperó a que el usuario completara la acción y, una vez hecho esto, el then() se ejecutó y llamó a mainMenu, devolviendo al usuario al menú principal.
+
+3. ¿Qué diferencias encontraste entre async, await y el método then()?
+
+Una de las principales diferencias es que con el uso de async/await, el código puede ser más fácil de seguir y entender y los dos se basan en promesas y no noté diferencias en rendimiento.
--- Tasks.js
+++ Tasks.js
@@ -8,39 +8,57 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-function addTask() {
+/*function addTask() {
   rl.question("Enter task description: ", (description) => {
     tasks.push({ description, completed: false });
     console.log(chalk.green("Task added successfully."));
     mainMenu();
   });
+}*/
+
+function addTask() {
+  return new Promise((resolve) => {
+    rl.question("Enter task description: ", (description) => {
+      tasks.push({ description, completed: false });
+      console.log(chalk.green("Task added successfully."));
+      resolve();
+    });
+  });
 }
 
 function removeTask() {
-  tasks.forEach((task, index) => {
-    console.log(
-      `${index + 1}. ${task.description} ${task.completed ? "(completed)" : ""}`
-    );
-  });
+  return new Promise((resolve) => {
+    tasks.forEach((task, index) => {
+      console.log(
+        `${index + 1}. ${task.description} ${
+          task.completed ? "(completed)" : ""
+        }`
+      );
+    });
 
-  rl.question("Enter the number of the task to remove: ", (num) => {
-    tasks.splice(num - 1, 1);
-    console.log(chalk.green("Task removed successfully."));
-    mainMenu();
+    rl.question("Enter the number of the task to remove: ", (num) => {
+      tasks.splice(num - 1, 1);
+      console.log(chalk.green("Task removed successfully."));
+      resolve();
+    });
   });
 }
 
 function completeTask() {
-  tasks.forEach((task, index) => {
-    console.log(
-      `${index + 1}. ${task.description} ${task.completed ? "(completed)" : ""}`
-    );
-  });
+  return new Promise((resolve) => {
+    tasks.forEach((task, index) => {
+      console.log(
+        `${index + 1}. ${task.description} ${
+          task.completed ? "(completed)" : ""
+        }`
+      );
+    });
 
-  rl.question("Enter the number of the task to complete: ", (num) => {
-    tasks[num - 1].completed = true;
-    console.log(chalk.green("Task completed successfully."));
-    mainMenu();
+    rl.question("Enter the number of the task to complete: ", (num) => {
+      tasks[num - 1].completed = true;
+      console.log(chalk.green("Task completed successfully."));
+      resolve();
+    });
   });
 }
 
@@ -52,6 +70,42 @@ function showTasks() {
   });
 }
 
+//1. asunc/await
+/*async function mainMenu() {
+  console.log(
+    "\n1. Add task\n2. Remove task\n3. Complete task\n4. Show tasks\n5. Exit"
+  );
+  rl.question("Choose an option: ", async (option) => {
+    switch (option) {
+      case "1":
+        await addTask();
+        mainMenu();
+        break;
+      case "2":
+        await removeTask();
+        mainMenu();
+        break;
+      case "3":
+        await completeTask();
+        mainMenu();
+        break;
+      case "4":
+        showTasks();
+        mainMenu();
+        break;
+      case "5":
+        console.log(chalk.blue("Goodbye!"));
+        rl.close();
+        break;
+      default:
+        console.log(chalk.red("Invalid option."));
+        mainMenu();
+    }
+  });
+}*/
+
+//2. then()
+
 function mainMenu() {
   console.log(
     "\n1. Add task\n2. Remove task\n3. Complete task\n4. Show tasks\n5. Exit"
@@ -59,13 +113,13 @@ function mainMenu() {
   rl.question("Choose an option: ", (option) => {
     switch (option) {
       case "1":
-        addTask();
+        addTask().then(mainMenu);
         break;
       case "2":
-        removeTask();
+        removeTask().then(mainMenu);
         break;
       case "3":
-        completeTask();
+        completeTask().then(mainMenu);
         break;
       case "4":
         showTasks();
